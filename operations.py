63143254e2bc6def901fcd142781fd80ae38ecf7
A01785840-AMD <angel.md@icloud.com>
2025-03-31 12:29:49 -0600

    Update operations.py

--- operations.py
+++ operations.py
@@ -2,7 +2,10 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
+print(f"Addition: {a + b}")
 
 # Display the subtraction
+print(f"Subtraction {a - b}")
 
 # Display multiplication
+print(f"multiplication {a * b}")
